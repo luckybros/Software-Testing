2c200c67458ab5529dbae14c00b8a53d2907461e
luca <luca.scolletta@outlook.it>
2023-11-17 09:46:57 +0100

    Aggiunta nuova classe da testare

---
+++ FontInfo/test/test/StudentTest.java
@@ -0,0 +1,119 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StudentTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFontInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFontInfoFont() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFamily() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFamily() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsBold() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIsBold() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsItalic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIsItalic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFont() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDoesFontMatch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGenerateStyle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateFont() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
